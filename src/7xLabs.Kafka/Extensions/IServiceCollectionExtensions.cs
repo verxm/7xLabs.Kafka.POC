f575cc45b7af2b8a301472ceaa4c64e20a506c49
Davi Santos <davisantos448@gmail.com>
2025-04-10 14:30:00 -0300

    refactor(IServiceCollectionExtensions): uses consumer and producer instrumented in dependency injection.

--- src/7xLabs.Kafka/Extensions/IServiceCollectionExtensions.cs
+++ src/7xLabs.Kafka/Extensions/IServiceCollectionExtensions.cs
@@ -22,17 +22,16 @@ public static class IServiceCollectionExtensions
     internal static IServiceCollection AddKafkaProducer<TMessageKey, TMessageValue>(this IServiceCollection services, Action<ProducerConfig>? configureProducer = null)
     {
         var kafkaProducer = ProducerFactory.Create<TMessageKey, TMessageValue>(configureProducer);
-        // var instrumentedKafkaProducer = new InstrumentedProducer<TMessageKey, TMessageValue>(kafkaProducer);
+        var instrumentedKafkaProducer = new InstrumentedProducer<TMessageKey, TMessageValue>(kafkaProducer);
 
-        return services.AddSingleton<IProducer<TMessageKey, TMessageValue>>(kafkaProducer);
+        return services.AddSingleton<IProducer<TMessageKey, TMessageValue>>(instrumentedKafkaProducer);
     }
 
     internal static IServiceCollection AddKafkaConsumer<TMessageKey, TMessageValue>(this IServiceCollection services, Action<ConsumerConfig>? configureConsumer = null)
     {
         var kafkaConsumer = ConsumerFactory.Create<TMessageKey, TMessageValue>(configureConsumer);
-        // var instrumentedKafkaConsumer = new InstrumentedConsumer<TMessageKey, TMessageValue>(kafkaConsumer); // TODO: testar se chega no APM
-        
+        var instrumentedKafkaConsumer = new InstrumentedConsumer<TMessageKey, TMessageValue>(kafkaConsumer);
 
-        return services.AddSingleton<IConsumer<TMessageKey, TMessageValue>>(kafkaConsumer);
+        return services.AddSingleton<IConsumer<TMessageKey, TMessageValue>>(instrumentedKafkaConsumer);
     }
 }
